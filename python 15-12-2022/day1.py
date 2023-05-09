f2ab5e8b4dd6a3c4e784ba03803b5ef4a0477107
vadiselapriya143@gmail.com <vadiselapriya143@gmail.com>
2023-05-09 15:31:21 +0530

    asdf

---
+++ python 15-12-2022/day1.py
@@ -0,0 +1,42 @@
+# DATA TYPES
+# integer
+a=20
+print(a)
+print(type(a))
+print(a+5)
+print(a-3)
+print(a*2)
+print(a/2)
+b=30
+print(b)
+print(a+b)
+print(a-b)
+print(a*b)
+print(a/b)
+print(a*2+b*2)
+# float data type
+x=1.5
+print(type(x))
+print(x)
+print(x+a)
+print(x+b)
+print(x+1.5)
+print(x+2.8)
+y=5.6
+print(type(y))
+print(y)
+print(x+y)
+print(x-y)
+print(x*y)
+# complex
+A=2+5j
+print(A)
+print(type(A))
+# boolean
+p=True
+s=False
+print(p)
+print(s)
+print(p+s)
+print(-16*4+True-5*2+False)
+print(True*5+False*3+4*5)
---
+++ python 15-12-2022/day2.py
@@ -0,0 +1,17 @@
+s='priyanka'
+print(s)
+print(type(s))
+p="bangaram"
+print(p)
+s="i love's you "
+print(s)
+# p='i love's bm'
+# print(p)
+# SyntaxError: unterminated string literal (detected at line 8)
+h='abcdefgh'
+k='1234567'
+print(h+k)
+print(h+k)
+
+
+
